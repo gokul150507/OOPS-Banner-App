eaffc15b910713e033d9e1a0fa9349cd08ff1613
gokul50507 <gokulr2247@gmail.com>
2026-02-16 09:29:00 -0800

    "OOPS Banner App feature UC1 Added"

---
+++ OPPS_Banner_App.java
@@ -0,0 +1,16 @@
+/**
+* 0OPSBannerApp UC1 1
+00PS Banner Display Application
+*
+* This class demonstrates a simple Java application that displays the 0bject
+* Oriented Programming System OOPS acronym to the console.
+*
+* @author Developer
+* @version 1.0
+*/
+public class 00PSBannerApp {
+    Run Debug
+    public static void main(String[] args) {
+        System.out.println(x:"0OPS");
+    }
+}

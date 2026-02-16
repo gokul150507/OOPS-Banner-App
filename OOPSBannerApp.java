9195c97eb94025a3979005241f236b4cf1fa1097
gokul50507 <gokulr2247@gmail.com>
2026-02-16 10:43:08 -0800

    "fix: Corrected ASCII Art"

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -11,23 +11,15 @@ public class OOPSBannerApp {
     public static void main(String[] args) {
         
         // Line 1: Top caps of O, O, P and S
-        System.out.println("   *** *** ****** *****");
-        // Line 2: Upper curves/shoulders
-        System.out.println(" ** ** ** ** ** ** ** ");
-        // Line 3: Vertical sides
-        System.out.println("** ** ** ** ** ** ** ");
-        // Line 4: Vertical sides
-        System.out.println("** ** ** ** ** ** ** ");
-        // Line 5: Middle bar for P and S
-        System.out.println("** ** ** ** ****** *** ");
-        // Line 6: Lower vertical sides
-        System.out.println("** ** ** ** ** ** ");
-        // Line 7: Lower vertical sides
-        System.out.println("** ** ** ** ** **");
-        // Line 8: Bottom curves
-        System.out.println(" ** ** ** ** ** ** **");
-        // Line 9: Bottom caps
-        System.out.println("   *** *** ** ***** ");
-        
+        System.out.println("   ****    " + " " + "   ****    " + " " + "**********  "+ " " + "**********");
+        System.out.println(" **     ** " + " " + " **     ** " + " " + "*********** "+ " " + "**********");
+        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**        **"+ " " + "**        ");
+        System.out.println("**       **" + " " + "**       **" + " " + "**       ** "+ " " + "**********");
+        System.out.println("**       **" + " " + "**       **" + " " + "*********   "+ " " + "**********");
+        System.out.println("**       **" + " " + "**       **" + " " + "***         "+ " " + "        **");
+        System.out.println(" **     ** " + " " + " **     ** " + " " + "***         "+ " " + "        **");
+        System.out.println("   ****    " + " " + "   ****    " + " " + "***         "+ " " + "**********");
+
     }
 }

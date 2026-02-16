d585487a9bc0ae87eaba2b98a14b87bfc64e3b6a
gokul50507 <gokulr2247@gmail.com>
2026-02-16 10:18:41 -0800

    "UC2 First commit"

---
+++ OOPSBannerApp.java
@@ -0,0 +1,33 @@
+/**
+ * OOPSBannerApp UC2 - Banner Display Application
+ * * This class displays a stylized "OOPS" banner in the console.
+ * The spacing is carefully calibrated to match the visual layout 
+ * of the provided reference image.
+ * * @author Developer
+ * @version 2.1
+ */
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+        
+        // Line 1: Top caps of O, O, P and S
+        System.out.println("   *** *** ****** *****");
+        // Line 2: Upper curves/shoulders
+        System.out.println(" ** ** ** ** ** ** ** ");
+        // Line 3: Vertical sides
+        System.out.println("** ** ** ** ** ** ** ");
+        // Line 4: Vertical sides
+        System.out.println("** ** ** ** ** ** ** ");
+        // Line 5: Middle bar for P and S
+        System.out.println("** ** ** ** ****** *** ");
+        // Line 6: Lower vertical sides
+        System.out.println("** ** ** ** ** ** ");
+        // Line 7: Lower vertical sides
+        System.out.println("** ** ** ** ** **");
+        // Line 8: Bottom curves
+        System.out.println(" ** ** ** ** ** ** **");
+        // Line 9: Bottom caps
+        System.out.println("   *** *** ** ***** ");
+        
+    }
+}
